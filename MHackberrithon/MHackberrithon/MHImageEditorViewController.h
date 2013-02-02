c5a4aec4020f43b5eeb256ec71e5839e63192137
LingyongWang <lwang84@illinois.edu>
2013-02-02 10:06:09 -0600

    test

---
+++ MHackberrithon/MHackberrithon/MHImageEditorViewController.h
@@ -0,0 +1,13 @@
+//
+//  MHImageEditorViewController.h
+//  MHackberrithon
+//
+//  Created by Lingyong Wang on 2/2/13.
+//  Copyright (c) 2013 Hackberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MHImageEditorViewController : UIViewController
+
+@end
---
+++ MHackberrithon/MHackberrithon/MHImageEditorViewController.m
@@ -0,0 +1,38 @@
+//
+//  MHImageEditorViewController.m
+//  MHackberrithon
+//
+//  Created by Lingyong Wang on 2/2/13.
+//  Copyright (c) 2013 Hackberry. All rights reserved.
+//
+
+#import "MHImageEditorViewController.h"
+
+@interface MHImageEditorViewController ()
+
+@end
+
+@implementation MHImageEditorViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
